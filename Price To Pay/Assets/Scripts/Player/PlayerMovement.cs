e1b18fca1e927fcdddb1fe7ff3e6599a303b44e8
Illia <52674359+Ilaay@users.noreply.github.com>
2022-07-10 12:47:37 +0300

    PlayerMovement update

--- Price To Pay/Assets/Scripts/Player/PlayerMovement.cs
+++ Price To Pay/Assets/Scripts/Player/PlayerMovement.cs
@@ -5,11 +5,24 @@ namespace Player
     public class PlayerMovement : MonoBehaviour
     {
         [SerializeField] private float moveSpeed;
-        
+
         private SpriteRenderer _spriteRenderer;
-        private Vector3 _moveDelta;
-        private Transform _transform;
         private Animator _animator;
+        private Vector3 _moveDelta;
+
+        #region Cached Properties
+
+        private int _currentState;
+
+        private static readonly int IdleSide = Animator.StringToHash("Idle Side Main Character Animation");
+        private static readonly int IdleUp = Animator.StringToHash("Idle Up Main Character Animation");
+        private static readonly int IdleDown = Animator.StringToHash("Idle Down Main Character Animation");
+
+        private static readonly int WalkSide = Animator.StringToHash("Walk Side Main Character Animation");
+        private static readonly int WalkUp = Animator.StringToHash("Walk Up Main Character Animation");
+        private static readonly int WalkDown = Animator.StringToHash("Walk Down Main Character Animation");
+
+        #endregion
 
         private void Awake()
         {
@@ -24,43 +37,51 @@ namespace Player
 
             _moveDelta = new Vector3(x, y, 0);
 
-            /*_spriteRenderer.flipX = _moveDelta.x switch
-            {
-                > 0 => true,
-                < 0 => false,
-                _ => _spriteRenderer.flipX
-            };*/
-            if(_moveDelta.x > 0)
-            {
-                _animator.SetInteger("MoveDirection", 2);
-                _spriteRenderer.flipX = false;
-
-            }
-            else if (_moveDelta.x < 0)
-            {   
-                _animator.SetInteger("MoveDirection", 2);
-                _spriteRenderer.flipX = true;
-            }
-            else
-            {
-                _animator.SetInteger("MoveDirection", 0);
-            }
-
-            if (_moveDelta.y > 0)
-            {
-                _animator.SetInteger("MoveDirection", 3);
-
-            }
-            else if (_moveDelta.y < 0)
-            {
-                _animator.SetInteger("MoveDirection", 1);
-            }
-            else
-            {
-                _animator.SetInteger("MoveDirection", 0);
-            }
-
+            if (_moveDelta.x != 0) _spriteRenderer.flipX = _moveDelta.x < 0;
             transform.Translate(_moveDelta * (moveSpeed * Time.deltaTime));
+
+            var state = GetState();
+
+            if (state == _currentState) return;
+            _animator.CrossFade(state, 0, 0);
+            _currentState = state;
+        }
+
+        private int GetState()
+        {
+            if (_moveDelta.x == 0 && _moveDelta.y == 0)
+            {
+                return ReturnLastPlayerDirection();
+            }
+
+            if (_moveDelta.x != 0)
+            {
+                return WalkSide;
+            }
+
+            if (_moveDelta.y != 0 && _moveDelta.x == 0)
+            {
+                switch (_moveDelta.y)
+                {
+                    case > 0:
+                        return WalkUp;
+                    case < 0:
+                        return WalkDown;
+                }
+            }
+
+            return IdleSide;
+        }
+
+        private int ReturnLastPlayerDirection()
+        {
+            if (_currentState == WalkSide) return IdleDown;
+            if (_currentState == WalkDown) return IdleDown;
+            if (_currentState == WalkUp) return IdleUp;
+            if (_currentState == IdleUp) return IdleUp;
+            if (_currentState == IdleDown) return IdleDown;
+
+            return IdleSide;
         }
     }
 }
