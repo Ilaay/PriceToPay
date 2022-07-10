96dffe9f7d70a5b3ec3a52a5ccd983c295f9e03a
YareDazai <yaroslavkozubua@gmail.com>
2022-07-10 11:26:58 +0300

    2D Game Environment Main Character animator updated

--- Price To Pay/Assets/Scripts/Player/PlayerMovement.cs
+++ Price To Pay/Assets/Scripts/Player/PlayerMovement.cs
@@ -9,10 +9,12 @@ namespace Player
         private SpriteRenderer _spriteRenderer;
         private Vector3 _moveDelta;
         private Transform _transform;
+        private Animator _animator;
 
         private void Awake()
         {
             _spriteRenderer = this.gameObject.GetComponent<SpriteRenderer>();
+            _animator = this.gameObject.GetComponent<Animator>();
         }
 
         private void FixedUpdate()
@@ -22,12 +24,41 @@ namespace Player
 
             _moveDelta = new Vector3(x, y, 0);
 
-            _spriteRenderer.flipX = _moveDelta.x switch
+            /*_spriteRenderer.flipX = _moveDelta.x switch
             {
                 > 0 => true,
                 < 0 => false,
                 _ => _spriteRenderer.flipX
-            };
+            };*/
+            if(_moveDelta.x > 0)
+            {
+                _animator.SetInteger("MoveDirection", 2);
+                _spriteRenderer.flipX = false;
+
+            }
+            else if (_moveDelta.x < 0)
+            {   
+                _animator.SetInteger("MoveDirection", 2);
+                _spriteRenderer.flipX = true;
+            }
+            else
+            {
+                _animator.SetInteger("MoveDirection", 0);
+            }
+
+            if (_moveDelta.y > 0)
+            {
+                _animator.SetInteger("MoveDirection", 3);
+
+            }
+            else if (_moveDelta.y < 0)
+            {
+                _animator.SetInteger("MoveDirection", 1);
+            }
+            else
+            {
+                _animator.SetInteger("MoveDirection", 0);
+            }
 
             transform.Translate(_moveDelta * (moveSpeed * Time.deltaTime));
         }
